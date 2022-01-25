a54ada875293e611f17c78523e79e8a655594342
Sunsun Kasajoo <st122283@ait.asia>
2022-01-25 17:44:41 +0700

    lab 2 completed

---
+++ lab2/button/assignment.js
@@ -0,0 +1,6 @@
+let showTeam = document.getElementById("show-team-btn")
+let myName = document.getElementById("show-name")
+
+showTeam.addEventListener("click", () => {
+    myName.innerHTML = "Sunsun";
+})
---
+++ lab2/index.php
@@ -0,0 +1,6 @@
+<HTML>
+<BODY>
+<H1>Hello from PHP!</H1>
+<?php echo("If you see this then PHP must be running") ?>
+</BODY>
+</HTML>
